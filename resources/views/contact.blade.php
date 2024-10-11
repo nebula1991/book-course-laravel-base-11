bf02b6f422d51b545e688905c9864451f3c030bf
nebula1991 <dbellanco3@gmail.com>
2024-10-12 00:32:47 +0200

    Migraciones

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,28 @@
+@extends('master')
+
+@section('content')
+
+<h1>Contact 1</h1>
+<p>{{ $posts[0] }}</p>
+
+
+
+
+{{-- @if ($name != "david")
+    <h2>Tu nombre no es Daniel </h2>
+ @else
+    <h2>Tu nombre es David </h2>
+@endif
+
+<ul>
+    @foreach ([1,2,3,4,5] as $item)
+    <li>{{ $item }}</li>
+    @endforeach
+</ul> --}}
+
+@endsection
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
+use App\Http\Controllers\PrimerControlador;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('test', [PrimerControlador::class,'index']);
+// Route::get('otro/{post}/{otro}', [PrimerControlador::class,'otro']);
+
+// Route::resource('post', PrimerControlador::class);
+
+
+
+
+
+
+
+
+
+
+
+/* Pruebas iniciales
+Route::get('/contact', function () {
+
+    #return redirect('/contact2', 303);
+    #return redirect()->route('contact2');
+    #return to_route('contact2');
+
+    return view('contact', ['name' => 'David']);
+})->name('contact');
+
+
+Route::get('/contact2', function () {
+    return view('contact2');
+})->name('contact2');
+*/
